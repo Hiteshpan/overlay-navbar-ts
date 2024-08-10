e0f4cee6cbd9dfc6e38f414688bb09e238eeb88c
Hiteshpan <hiteshrtk999@gmail.com>
2024-08-11 00:14:28 +0530

    Button fixed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,19 +3,22 @@ import { ReactNode } from 'react'
 
 type PropsType = {
     children: ReactNode;
+    onClick?: () => void;
 };
 
-const Button = ({ children }: PropsType) => {
+const Button = ({ children, onClick }: PropsType) => {
     return (
-        <button style={{
-            padding: "10px 20px",
-            fontSize: "1.2em",
-            borderRadius: "5px",
-            cursor: "pointer",
-            backgroundColor: "blue",
-            color: "white",
-            border: "none",
-        }}
+        <button
+            onClick={onClick}
+            style={{
+                padding: "10px 20px",
+                fontSize: "1.2em",
+                borderRadius: "5px",
+                cursor: "pointer",
+                backgroundColor: "blue",
+                color: "white",
+                border: "none",
+            }}
         >
             {children}
 
